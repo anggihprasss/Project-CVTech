fd75c0054e430c6d8e43194efc8b21c95eedf955
Anggih Prasetio <anggihprasetio77@upi.edu>
2024-06-03 15:34:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-# Import Module
+'''
+    Contoh Deloyment untuk Domain Computer Vision (CV)
+    Orbit Future Academy - AI Jobs - MBKM Batch 6
+    Tim Deployment
+    2024
+'''
 
+# Import Module
 import numpy as np
-import PIL
 from PIL import Image, ImageOps
 import tensorflow as tf
 from tensorflow import keras
@@ -10,7 +15,7 @@ import cv2
 import time
 import os
 import base64
-from base64 import b64encode
+
 
 # Class names dan explanation
 class_names=['Blight', 'Common Rust (Karatan Umum)', 'Gray Leaf Spot (Bercak Daun Abu-abu)', 'Sehat']
@@ -23,8 +28,6 @@ class_explanations={
         "3. Pengaturan Jarak Tanam: Menjaga jarak yang cukup antara tanaman jagung dapat membantu meningkatkan sirkulasi udara di antara tanaman, mengurangi kelembaban, dan mengurangi risiko penyebaran Blight.",
         "4. Pengendalian Hama Penghambat: Beberapa hama seperti kutu daun atau ulat daun dapat melemahkan tanaman jagung, membuatnya lebih rentan terhadap infeksi Blight. Pengendalian hama ini dapat membantu menjaga kesehatan tanaman dan mengurangi risiko infeksi tambahan.",
         "5. Penggunaan Fungisida: Penggunaan fungisida dapat menjadi pilihan terakhir jika infeksi Blight sudah parah. Konsultasikan dengan ahli pertanian lokal atau petani lainnya untuk rekomendasi fungisida yang tepat dan cara aplikasinya.",
-        "6. Pengelolaan Tanah: Praktik-praktik seperti mengelola drainase tanah dengan baik dan menghindari kelembaban berlebihan di sekitar tanaman juga dapat membantu mengurangi risiko infeksi Blight.",
-        "7. Pemantauan dan Tindakan Cepat: Penting untuk memantau tanaman secara teratur untuk deteksi dini gejala Blight. Jika ada tanda-tanda infeksi, tindakan cepat harus diambil untuk membatasi penyebarannya, termasuk pemangkasan daun yang terinfeksi dan pembersihan sisa-sisa tanaman setelah panen.",
         "<a href='https://plantix.net/id/library/plant-diseases/100161/southern-leaf-blight-of-maize/'>Klik untuk informasi selanjutnya..."
     ],
     'Common Rust (Karatan Umum)': [
@@ -84,10 +87,6 @@ logo_right = os.path.join(image_dir, "logo_right.png")
 logo_left_base64 = image_to_base64(logo_left)
 logo_right_base64 = image_to_base64(logo_right)
 
-# Fungsi untuk mengubah gambar ke base64
-def image_to_base64(image_path):
-    with open(image_path, "rb") as image_file:
-        return b64encode(image_file.read()).decode()
 
 # Fungsi untuk menentukan gaya caption berdasarkan tema
 def get_caption_style():
@@ -128,18 +127,85 @@ html_temp = f"""
 # Tampilkan HTML di Streamlit
 st.markdown(html_temp, unsafe_allow_html=True)
 
+# Function to display copyright text
+def display_copyright():
+    st.markdown(
+        """
+        <div style="position: fixed; bottom: 10px; right: 10px; font-size: 12px; color: #888;">
+            Team 1 &copy; CVtech 2024
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+
+# Display copyright text
+display_copyright()
+
 # Sidebar
 st.sidebar.title("CVTech Application")
-app_mode = st.sidebar.selectbox("Pilihan", ["Tentang Aplikasi", "Tim Pengembang", "Sistem Prediksi"])
+app_mode = st.sidebar.selectbox("Pilihan", ["Halaman Depan", "Tentang Aplikasi", "Tim Pengembang", "Sistem Prediksi"])
 
-# Tentang Projek
-if app_mode == "Tentang Aplikasi":
+# Halaman Depan
+if app_mode == "Halaman Depan":
     st.markdown("""
     <h2 style="text-align: center; font-size: 32px;">Corn Vision Technology: Deteksi Dini, Panen Pasti!</h2>
     <div style="text-align: justify; font-size: 18px;">
     Pertanian merupakan tulang punggung bagi pemenuhan kebutuhan pangan global, namun petani sering kali dihadapkan pada tantangan yang kompleks. Tanaman jagung, salah satu tanaman pangan utama, tidak luput dari berbagai ancaman penyakit dan gangguan yang dapat mengancam hasil panen. Untuk mengatasi masalah ini, CVTech hadir sebagai solusi terdepan dengan menggunakan teknologi terkini dalam bidang Computer Vision.
     </div>
     """, unsafe_allow_html=True)
+
+# Tentang Aplikasi
+if app_mode == "Tentang Aplikasi":
+    st.markdown("""
+    <h2 style="text-align: center; font-size: 32px;">Tentang Aplikasi</h2>
+    <div style="text-align: center; font-size: 15px;">
+    CVTech adalah aplikasi yang menggunakan teknologi Computer Vision untuk mendeteksi penyakit pada tanaman jagung. Dengan memanfaatkan model pembelajaran machine learning, aplikasi ini dapat mengenali berbagai penyakit jagung hanya dengan menganalisis gambar daun jagung.
+    </div>
+    """, unsafe_allow_html=True)
+    
+    st.markdown("""
+    <style>
+    .image-container {
+        text-align: center;
+        margin-top: 20px;
+        margin-bottom: 20px;
+    }
+    .caption {
+        font-size: 13px; /* Ubah ukuran caption di sini */
+        margin-top: 2px;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+    
+    # Gambar dan Caption
+    col1, col2, col3 = st.columns(3)
+    
+    with col1:
+        st.markdown(f"""
+        <div class="image-container">
+            <img src='data:image/png;base64,{image_to_base64("images/aman.png")}' alt='AMAN' width='150' style='display: block; margin: 5px auto;'>
+            <div class="caption">Privasi dan perlindungan informasi Anda adalah prioritas utama kami.</div>
+        </div>
+        """, unsafe_allow_html=True)
+        
+    with col2:
+        st.markdown(f"""
+        <div class="image-container">
+            <img src='data:image/png;base64,{image_to_base64("images/akurat.png")}' alt='AKURAT' width='150' style='display: block; margin: 5px auto;'>
+            <div class="caption">Akurasi yang tepat memungkinkan anda menemukan solusi dengan cepat.</div>
+        </div>
+        """, unsafe_allow_html=True)
+    
+    with col3:
+        st.markdown(f"""
+        <div class="image-container">
+            <img src='data:image/png;base64,{image_to_base64("images/mudah.png")}' alt='MUDAH' width='150' style='display: block; margin: 5px auto;'>
+            <div class="caption">Dapat digunakan kapan saja dan dimana saja.
+            </div>
+        </div>
+        """, unsafe_allow_html=True)
+
+
 
 # Tim Pengembang
 elif app_mode == "Tim Pengembang":
@@ -148,32 +214,32 @@ elif app_mode == "Tim Pengembang":
     # Fungsi untuk menampilkan profil anggota
     def show_profile(image_path, name, univ, role, github_link, linkedin_link, margin_top='5px'):
         st.markdown(f"""
-        <div style="text-align: center; width: 200px; margin: 0 auto;">
-            <img src='data:image/png;base64,{image_to_base64(image_path)}' width='128' style='display: block; margin: {margin_top} auto 5px;'>
-            <div style='margin-bottom: 5px;'><span style='font-size: 18px;'>{name}</span></div>
+        <div style="text-align: center; width: 250px; margin: 0 auto;">
+            <img src='data:image/png;base64,{image_to_base64(image_path)}' width='150' style='display: block; margin: {margin_top} auto 5px;'>
+            <div style='margin-bottom: -3px;'><span style='font-size: 16px;'>{name}</span></div>
             <div style='margin: 0 auto;'>
-                <p style='margin-top: 2px; margin-bottom: 2px;'><strong></strong> {univ}</p>
-                <p><strong></strong> {role}</p>
+                <p style='margin-top: 0px; margin-bottom: -3px; font-size: 13px;'><strong></strong> {univ}</p>
+                <p style='margin-top: 0px; margin-bottom: 1px; font-size: 13px;''><strong></strong> {role}</p>
                 <p>
-                <a href="{github_link}" target="_blank"><img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" alt="GitHub" width="30" style="margin-right: 10px;"></a> 
-                <a href="{linkedin_link}" target="_blank"><img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png" alt="LinkedIn" width="30"></a>
+                <a href="{github_link}" target="_blank"><img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" alt="GitHub" width="25" style="margin-right: 10px;"></a> 
+                <a href="{linkedin_link}" target="_blank"><img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png" alt="LinkedIn" width="25"></a>
                 </p>
             </div>
         </div>
         """, unsafe_allow_html=True)
     
-    # Membuat kolom untuk setiap baris
-    col1, col2 = st.columns(2)
-    with col1:
-        show_profile('images/photo_anggota1.png', 'Anggih Prasetio', 'Universitas Pendidikan Indonesia', 'Data Scientist', 'https://github.com/anggihprasss', 'https://www.linkedin.com/in/anggih-prasetio-a32218223/', margin_top='20px')
-    with col2:
-        show_profile('images/photo_anggota2.png', 'Asyifa Fauziyah', 'Institut Teknik Garut', 'Data Engineer', 'https://github.com/Asyifauziyah990', 'https://www.linkedin.com/', margin_top='20px')
+    # Membuat kolom untuk setiap anggota tim
+    col1, col2, col3, col4 = st.columns(4)
     
-    col3, col4 = st.columns(2)
+    with col1:
+        show_profile('images/photo_anggota1.png', 'Anggih Prasetio', 'Universitas Pendidikan Indonesia', 'Data Scientist', 'https://github.com/anggihprasss', 'https://www.linkedin.com/in/anggih-prasetio-a32218223/')
+    with col2:
+        show_profile('images/photo_anggota2.png', 'Asyifa Fauziyah', 'Institut Teknik Garut', 'Data Engineer', 'https://github.com/Asyifauziyah990', 'https://www.linkedin.com/')
     with col3:
-        show_profile('images/photo_anggota3.png', 'Muhammad Raihan', 'Institut Teknik Garut', 'Full Stack Developer', 'https://github.com/Reyourbae', 'https://www.linkedin.com/', margin_top='40px')
+        show_profile('images/photo_anggota3.png', 'Muhammad Raihan', 'Institut Teknik Garut', 'Full Stack Developer', 'https://github.com/Reyourbae', 'https://www.linkedin.com/in/muhammad-raihan-6680112a3/')
     with col4:
-        show_profile('images/photo_anggota4.png', 'Nabila Fitriyani', 'Universitas Mayasari Bakti', 'Project Manager', 'https://github.com/nbilaft', 'https://www.linkedin.com/', margin_top='40px')
+        show_profile('images/photo_anggota4.png', 'Nabila Fitriyani', 'Universitas Mayasari Bakti', 'Project Manager', 'https://github.com/nbilaft', 'https://www.linkedin.com/')
+
 
 # Sistem Deteksi
 elif app_mode == "Sistem Prediksi":
@@ -194,23 +260,17 @@ elif app_mode == "Sistem Prediksi":
         st.text("Silahkan Unggah Gambar")
     else:
         image = Image.open(file)
-        st.image(image, width=200)
+        st.image(image, width=300)
         if st.button("Prediksi"):
-            countdown = 3  # waktu hitung mundur dalam detik
-            countdown_placeholder = st.empty()
-            for i in range(countdown, 0, -1):
-                countdown_placeholder.text(f'Memproses... {i} detik tersisa')
+            with st.spinner('Sedang memproses...'):
                 time.sleep(3)
-            countdown_placeholder.empty()
-            predictions = import_and_predict(image, model)
-            score = np.array(predictions[0])
-            predicted_class = class_names[np.argmax(score)]
-            confidence = np.max(score)  # Mendapatkan nilai keyakinan tertinggi
-
-            st.title("Sistem memprediksi jagung dalam kondisi {}".format(predicted_class))
-            st.write("Penjelasan:")
-            explanation_html = "<div class='justified'>"
-            for point in class_explanations[predicted_class]:
-                explanation_html += "<p>{}</p>".format(point)
-            explanation_html += "</div>"
-            st.markdown(explanation_html, unsafe_allow_html=True)
+                predictions = import_and_predict(image, model)
+                score = np.array(predictions[0])
+                predicted_class = class_names[np.argmax(score)]
+                st.title("Sistem memprediksi jagung dalam kondisi {}".format(predicted_class))
+                st.write("Penjelasan:")
+                explanation_html = "<div class='justified'>"
+                for point in class_explanations[predicted_class]:
+                    explanation_html += "<p>{}</p>".format(point)
+                explanation_html += "</div>"
+                st.markdown(explanation_html, unsafe_allow_html=True)
